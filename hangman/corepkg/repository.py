37b574305980b26ce48057d7268365142e643516
sykoh1205 <sykoh1205@naver.com>
2025-06-04 12:06:55 +0900

    feat: 단어장 단어 추가

--- hangman/corepkg/repository.py
+++ hangman/corepkg/repository.py
@@ -1,19 +1,27 @@
 word_list_animals = [
     "dog", "cat", "pig", "cow", "bat", "ant", "bee", "fox", "rat", "duck",
     "frog", "fish", "goat", "bear", "lion", "tiger", "zebra", "horse", "sheep", "mouse",
-    "crab", "deer", "bird", "whale", "shark", "snail", "snake", "monkey", "panda", "camel"
+    "crab", "deer", "bird", "whale", "shark", "snail", "snake", "monkey", "panda", "camel",
+    "donkey", "owl", "puppy", "kitten", "turkey", "swan", "seal", "lizard", "beetle", "weasel",
+    "chicken", "mole", "reindeer", "goldfish", "parrot", "hamster", "rabbit", "squirrel", "otter",
+    "kangaroo", "koala", "chameleon", "armadillo", "dolphin", "crocodile", "alligator"
 ]
 
 word_list_foods = [
     "egg", "rice", "milk", "meat", "corn", "cake", "soup", "salt", "bean", "fish",
     "pork", "beef", "yogurt", "juice", "bread", "apple", "grape", "peach", "lemon", "melon",
-    "sugar", "honey", "candy", "noodle", "water", "snack", "cheese", "pizza", "donut", "chips"
+    "sugar", "honey", "candy", "noodle", "water", "snack", "cheese", "pizza", "donut", "chips",
+    "toast", "butter", "cookie", "jam", "cereal", "icecream", "burger", "fries", "spaghetti", "ham",
+    "carrot", "olive", "ketchup", "cucumber", "pepper", "garlic", "salad", "eggplant", "syrup",
+    "pasta", "taco", "sushi", "waffle", "pancake", "popcorn", "coconut", "muffin", "curry"
 ]
 
 word_list_sports = [
     "run", "swim", "ski", "golf", "yoga", "judo", "hike", "walk", "jump", "dive",
     "kick", "race", "dance", "tennis", "soccer", "boxing", "skate", "rugby", "karate", "ping",
-    "pong", "catch", "throw", "slide", "climb", "surf", "shoot", "vault", "relay", "fence"
+    "pong", "catch", "throw", "slide", "climb", "surf", "shoot", "vault", "relay", "fence",
+    "basketball", "baseball", "football", "volleyball", "cricket", "badminton", "hockey", "lacrosse",
+    "wrestling", "tabletennis", "softball", "squash", "handball", "waterpolo", "bowling", "cycling"
 ]
 
 hangman_pics = [
